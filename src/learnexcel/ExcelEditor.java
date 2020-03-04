fb3cb9ad40cb164abe328b96bd50d7b036da6fef
NizhnikovEvgeny <nizhnikovevgeny@gmail.com>
2020-03-04 19:10:55 +0300

    Added math

--- src/learnexcel/ExcelEditor.java
+++ src/learnexcel/ExcelEditor.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.HashMap;
+import org.apache.commons.math3.stat.correlation.Covariance;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 public class ExcelEditor {
@@ -45,37 +46,7 @@ public class ExcelEditor {
     void createNewBook() throws IOException {
         Workbook MyWB = new XSSFWorkbook();
         Path file_path = FileSystems.getDefault().getPath("Cov.xlsx");
-        int n=MyExport.get("X").length;
-        double Mx=0,My=0,Mz=0;
-        double sum=0;
-        for (double value : MyExport.get("X")) {
-            sum+=value;
-        } 
-        Mx=sum/n;
-        sum=0;
-        for (double value : MyExport.get("Y")) {
-            sum+=value;
-        }
-        My=sum/n;
-        sum=0;
-        for (double value : MyExport.get("Z")) {
-            sum+=value;
-        }
-        Mz=sum/n;
-        double covXY=0,covXZ=0,covYZ=0;
-        sum=0;
-        double valX=0,valY=0,valZ=0;
-        for (int i=0;i<n;i++){
-            valX = MyExport.get("X")[i] - Mx;
-            valY = MyExport.get("Y")[i] - My;
-            valZ = MyExport.get("Z")[i] - Mz;
-            covXY+=valX*valY;
-            covXZ+=valX*valZ;
-            covYZ+=valY*valZ;
-        }
-        covXY/=(n-1);
-        covXZ/=(n-1);
-        covYZ/=(n-1);
+        Covariance cov = new Covariance();
         Sheet MyFirstSheet = MyWB.createSheet("Первый лист");
         Row MyFirstRow = MyFirstSheet.createRow(0);
         Row MySecondRow = MyFirstSheet.createRow(1);
@@ -89,9 +60,9 @@ public class ExcelEditor {
         name1.setCellValue("Ковариация XY");
         name2.setCellValue("Ковариация XZ");
         name3.setCellValue("Ковариация YZ");
-        value1.setCellValue(covXY);
-        value2.setCellValue(covXZ);
-        value3.setCellValue(covYZ);
+        value1.setCellValue(cov.covariance(MyExport.get("X"),MyExport.get("Y")));
+        value2.setCellValue(cov.covariance(MyExport.get("X"),MyExport.get("Z")));
+        value3.setCellValue(cov.covariance(MyExport.get("Y"),MyExport.get("Z")));
         
         FileOutputStream stream = new FileOutputStream(new File(file_path.toString()));
         MyWB.write(stream);
